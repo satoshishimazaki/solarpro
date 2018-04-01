064a0a55f1b4731c258820100c5e7fe72741a048
satoshishimazaki <sattori06010601@gmail.com>
2018-04-01 11:47:02 +0900

    Add title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def full_title(page_title='')
+		base_title = "太陽光発電メンテナンスなら太陽光メンテプロ"
+		if current_page?(:controller => "inquiries", :action => "new")
+			"太陽光発電メンテナンス専門の業者紹介・選定サービス　太陽光メンテプロ"
+		else 
+			if page_title.empty?
+				base_title
+			else
+				page_title + "|" + base_title
+			end
+		end
+	end
 end
