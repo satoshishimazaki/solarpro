90731209af658933fd15bf7731859dfa85dfe344
satoshishimazaki <sattori06010601@gmail.com>
2018-04-01 12:07:53 +0900

    Repair title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def full_title(page_title='')
-		base_title = "太陽光発電メンテナンスなら太陽光メンテプロ"
+		base_title = "太陽光メンテナンス、点検、洗浄、草刈りなら太陽光メンテプロ"
 		if current_page?(:controller => "inquiries", :action => "new")
-			"太陽光発電メンテナンス専門の業者紹介・選定サービス　太陽光メンテプロ"
+			"太陽光メンテナンス、点検、洗浄、草刈り専門の業者紹介サービス　太陽光メンテプロ"
 		else 
 			if page_title.empty?
 				base_title
